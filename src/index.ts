679302c5c082b32042ac3f508eb4392edab62de7
Danielle_Atherfold <datherfold16578@alumni.focuscollege.com>
2021-03-10 17:32:49 -0800

    made a class and a conductor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 console.log("Software Developmnet: Object Oriented Programming")
+class Person{
+    Id:number
+    FirstName:string
+    LastName:string
+    Name:string
+
+    public constructor(theName: string) {
+        this.Name = theName;
+    }
+}
+
