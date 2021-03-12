460d9e2c0d5c0dabd6cae4995747e364c25ddf5b
Danielle_Atherfold <datherfold16578@alumni.focuscollege.com>
2021-03-11 18:49:21 -0800

    exercise5

--- src/class.property.ts
+++ src/class.property.ts
@@ -1,9 +1,16 @@
 class person {
 }
 
+
+
 export class Property {
-readonly address:string;
-id:string;
-owner:person;
+    readonly address: string;
+    id: string;
+    owner: person;
+
+    constructor(owner: person) {
+        this.owner = owner
+    }
 
 }
+
